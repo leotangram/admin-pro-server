6ac837eaff3e193edadd480fd0131d17102e026e
Leonardo Omaña <leotangram@gmail.com>
2019-06-14 10:02:58 -0500

    Connection between mongo and established node.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Initialize variables
 var app = express();
 
+// Conexión a la base de datos
+mongoose.connection.openUri(
+  'mongodb://localhost:27017/hospitalDB',
+  (error, response) => {
+    if (error) throw error;
+    console.log('Data base: \x1b[32m%s\x1b[0m', 'online');
+  }
+);
+
+// Routes
+app.get('/', (request, response, next) => {
+  response.status(200).json({
+    ok: true,
+    message: 'Petición realizada correctamente'
+  });
+});
+
 // Listen petitions
 app.listen(3000, () => {
-  console.log('Express server port 3000 \x1b[32m%s\x1b[0m','online');
+  console.log('Express server port 3000 \x1b[32m%s\x1b[0m', 'online');
 });
